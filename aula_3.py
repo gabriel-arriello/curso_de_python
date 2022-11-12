31e7e6ec2f1061ad71d140a2e9754fe798713875
gabriel-arriello <118053603+gabriel-arriello@users.noreply.github.com>
2022-11-12 13:03:52 -0300

    Add files via upload

---
+++ aula_3.py
@@ -0,0 +1,49 @@
+#importando módulos
+"""
+from math import ceil
+
+import math 
+
+#funções e retorno
+
+def soma(a,b)
+    result = a + b
+    return result
+
+resultado = soma(4,5)
+
+
+def delta(a,b,c):
+    
+    result=b**2 - 4*a*c
+    return result
+
+raiz = delta(1, -3, -10)
+
+if raiz >= 0:
+    print("Tenho raízes")
+else:
+    print("Não tenho raízes")
+
+#-b +- raiz(delta) / 2a
+
+raiz_um = (3 + sqrt(raiz))/2
+raiz_dois =(3 - sqrt(raiz))/2
+
+if raiz >= 0:
+    print("Possue raízes")
+else:
+    print("Não possue raízes")
+
+#documentação de código: sphinx
+
+#escopo de variável
+
+#quantidade de argumentos variáveis
+"""
+def perimetro(*args):
+    return sum(args)
+
+resultado = perimetro(40,20,50,15)
+print(resultado)
+
