a7da48939dd945c4cb1c0cba1ae6627439664184
gabriel-arriello <118053603+gabriel-arriello@users.noreply.github.com>
2022-11-12 13:06:24 -0300

    Add files via upload

---
+++ exercicio_4.py
@@ -0,0 +1,12 @@
+ano = 0
+pais_a = 80000
+pais_b = 200000
+
+while pais_a < pais_b:
+    pais_a = pais_a * 1.03
+    pais_b = pais_b * 1.015
+    ano = ano + 1
+
+ano_que_ultrapassa = ano
+    
+print(ano_que_ultrapassa)
