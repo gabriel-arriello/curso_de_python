773ce6db15780f137337c57d172d7de8efcbcc85
gabriel-arriello <118053603+gabriel-arriello@users.noreply.github.com>
2023-02-10 14:06:34 -0300

    Add files via upload

---
+++ seq_exercicio8.py
@@ -0,0 +1,7 @@
+
+salario_hora = float(input("Quanto você ganha por hora? "))
+qt_horas = float(input("Quantas horas você trabalhou no mês? "))
+
+salario = salario_hora * qt_horas
+
+print("Seu salário este mês foi: R$", round(salario, 2))
